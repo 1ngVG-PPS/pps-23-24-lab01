17bd02f131ef8783987a13d4a94c48a1264d08bb
Valerio-Giannini <valerio.giannini@studio.unibo.it>
2024-02-26 10:56:23 +0100

    Exercise 1 without DRY

--- basic-refactoring-exercise/src/main/java/example/Main.java
+++ basic-refactoring-exercise/src/main/java/example/Main.java
@@ -2,13 +2,14 @@ package example;
 
 import example.model.AccountHolder;
 import example.model.BankAccount;
-import example.model.SimpleBankAccount;
+//import example.model.SimpleBankAccount;
+import example.model.SimpleBankAccountWithAtm;
 
 public class Main {
 
     public static void main(String[] args) {
         final AccountHolder accountHolder = new AccountHolder("Mario", "Rossi", 1);
-        final BankAccount bankAccount = new SimpleBankAccount(accountHolder, 0);
+        final BankAccount bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0);
 
         bankAccount.deposit(accountHolder.getId(), 100);
 
---
+++ basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithAtm.java
@@ -0,0 +1,46 @@
+package example.model;
+
+public class SimpleBankAccountWithAtm implements BankAccount{
+    
+    private double balance;
+    private final AccountHolder holder;
+
+    public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    @Override
+    public AccountHolder getHolder(){
+        return this.holder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            this.balance += (amount - 1);//1$ Fee
+        }
+    }
+
+    @Override
+    public void withdraw(final int userID, final double amount) {
+        if (checkUser(userID) && isWithdrawAllowed(amount)) {
+            this.balance -= amount;
+            this.balance -= 1;
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return this.balance >= amount;
+    }
+
+    private boolean checkUser(final int id) {
+        return this.holder.getId() == id;
+    }
+   
+}
